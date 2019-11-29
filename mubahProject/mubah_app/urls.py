7adb37052174a694b1350dae31b7761b689128dc
zhantore <zhantore_15@mail.ru>
2019-11-29 23:23:28 +0600

    fifth commit from Zhantore

--- mubahProject/mubah_app/urls.py
+++ mubahProject/mubah_app/urls.py
@@ -1,4 +1,4 @@
-"""mubahAPIh URL Configuration
+"""mubah_app URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.2/topics/http/urls/
